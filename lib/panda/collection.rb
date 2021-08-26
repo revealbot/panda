cb8119ee24f6fb98bd2635e96e1f26a894521298
Mikhail Salosin <salosinm@gmail.com>
2021-08-26 15:40:00 +0300

    Paging info is actually inside data field

--- lib/panda/collection.rb
+++ lib/panda/collection.rb
@@ -14,10 +14,11 @@ module Panda
     def initialize(response, client)
       @response = response
       @client = client
-      @page = response.parsed_body.dig('page_info', 'page')
-      @page_size = response.parsed_body.dig('page_info', 'page_size')
-      @total_number = response.parsed_body.dig('page_info', 'total_number')
-      @total_page = response.parsed_body.dig('page_info', 'total_page')
+
+      @page = response.parsed_body.dig('data', 'page_info', 'page')
+      @page_size = response.parsed_body.dig('data', 'page_info', 'page_size')
+      @total_number = response.parsed_body.dig('data', 'page_info', 'total_number')
+      @total_page = response.parsed_body.dig('data', 'page_info', 'total_page')
 
       super response.parsed_body.dig('data', 'list')
     end
