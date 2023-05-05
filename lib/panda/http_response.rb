8b7969a9b8b8660d68b0694535fd6e4071815412
Marat Khaliullin <xalmar@gmail.com>
2023-05-05 17:15:09 +0200

    feat(ErrorHandler): remove redundant lines

--- lib/panda/http_response.rb
+++ lib/panda/http_response.rb
@@ -2,23 +2,13 @@
 
 module Panda
   class HTTPResponse
-    SUCCESS_CODE = 0
-
     attr_reader :status, :headers, :parsed_body, :request_id
 
     def initialize(status, headers, body)
       @status = status
       @headers = headers
-
-      parse(body)
-    end
-
-    private
-
-    def parse(body)
       @parsed_body = JSON.parse(body)
       @request_id = parsed_body['request_id']
-      return if parsed_body['code'] == SUCCESS_CODE
     end
   end
 end
